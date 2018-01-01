c8ced1327c674205bf776316f17907ccb6d147f2
Shamoun Syed <shamoun723@gmail.com>
2017-12-31 16:19:53 -0800

    feat: complete animation for list decks

--- components/ListDeck.js
+++ components/ListDeck.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { connect } from 'react-redux'
 
 class ListDeck extends Component {
 
+  state = {
+    bounceValue: new Animated.Value(1),
+  }
+
   render() {
     const { title, navigation } = this.props
     const count = this.props.decks[title].questions.length
+    const { bounceValue } = this.state
 
     return (
       <View style={{flex: 1}}>
         <TouchableOpacity
           style={[{flex: 1}, styles.deck]}
-          onPress={() => (navigation.navigate('Deck', { deckName: title }))}>
-          <Text style={styles.title}>{title}</Text>
+          onPress={() => {
+            Animated.sequence([
+              Animated.timing(bounceValue, { duration: 0, toValue: 1.5}),
+              Animated.spring(bounceValue, { toValue: 1, tension: 100})
+            ]).start(() => (navigation.navigate('Deck', { deckName: title })))
+          }}>
+          <Animated.Text style={[styles.title, {transform: [{scale: bounceValue}]}]}>{title}</Animated.Text>
           <Text style={styles.count}>{count === 1 ? count + " card" : count + " cards"}</Text>
         </TouchableOpacity>
       </View>
